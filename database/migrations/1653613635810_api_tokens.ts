da413328cb7f66d7024a9851bca1d1ff3a2e7f9c
Ewerton Dutra <ewerton-speed@hotmail.com>
2022-05-26 23:25:43 -0300

    aula 5 e 6

--- database/migrations/1653613635810_api_tokens.ts
+++ database/migrations/1653613635810_api_tokens.ts
@@ -10,12 +10,8 @@ export default class ApiTokens extends BaseSchema {
       table.string('name').notNullable()
       table.string('type').notNullable()
       table.string('token', 64).notNullable().unique()
-
-      /**
-       * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
-       */
-      table.timestamp('expires_at', { useTz: true }).nullable()
-      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('expires_at').nullable()
+      table.timestamp('created_at').notNullable()
     })
   }
 
