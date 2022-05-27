cbe6629c779768cee88beb2ddfb0e904ad450ee3
Ewerton Dutra <ewerton-speed@hotmail.com>
2022-05-27 00:39:54 -0300

    aula 8

--- database/migrations/1653613635800_users.ts
+++ database/migrations/1653613635800_users.ts
@@ -6,7 +6,7 @@ export default class UsersSchema extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.string('email', 255).notNullable()
+      table.string('email', 255).unique().notNullable()
       table.string('password', 180).notNullable()
       table.string('tipo', 20).notNullable()
       table.string('remember_me_token').nullable()
--- start/routes.ts
+++ start/routes.ts
@@ -1,3 +1,4 @@
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import Route from "@ioc:Adonis/Core/Route";
 import User from "App/Models/User";
 
@@ -8,3 +9,26 @@ Route.get("/", async () => {
     tipo: "admin",
   });
 });
+
+Route.post(
+  "/gettoken",
+  async ({ request, response, auth }: HttpContextContract) => {
+    const email = request.input("email");
+    const password = request.input("password");
+
+    const user = await User.findBy("email", email);
+
+    if (user == null) {
+      return response.notFound("Usuário não encontrado");
+    }
+
+    const token = await auth.use("api").attempt(email, password);
+
+    return response.ok(token);
+  }
+);
+
+Route.get("/auth", async ({ auth, response }: HttpContextContract) => {
+    return response.ok("somente usuários autenticados podem acessar");
+
+}).middleware("api");
