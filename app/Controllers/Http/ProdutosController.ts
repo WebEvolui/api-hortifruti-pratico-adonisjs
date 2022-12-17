6878ba45df6550923ea6ed787cf738936390f559
Ewerton Dutra <ewerton-speed@hotmail.com>
2022-12-17 01:50:48 -0300

    update aula 12 cadastro edicao e excluir produto

--- app/Controllers/Http/ProdutosController.ts
+++ app/Controllers/Http/ProdutosController.ts
@@ -1,3 +1,4 @@
+import { DateTime } from "luxon";
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import CreateEditProdutoValidator from "App/Validators/CreateEditProdutoValidator";
 import Drive from "@ioc:Adonis/Core/Drive";
@@ -33,11 +34,62 @@ export default class ProdutosController {
     return response.created(produto);
   }
 
-  public async update({}: HttpContextContract) {
-    return "Hello World!";
+  public async update({
+    request,
+    response,
+    bouncer,
+    params,
+  }: HttpContextContract) {
+    await bouncer.authorize("UserIsEstabelecimento");
+
+    const payload = await request.validate(CreateEditProdutoValidator);
+    const produto = await Produto.findOrFail(params.id);
+
+    await bouncer
+      .with("ProdutoPolicy")
+      .authorize("isOwner", produto.categoria_id);
+
+    if (payload.imagem) {
+      if (produto.imagem) {
+        const file = produto.imagem.split("/").filter(Boolean).pop();
+        if (file?.length) await Drive.delete(file);
+      }
+
+      await payload.imagem.moveToDisk("./");
+    }
+
+    let tmpProduct = {
+      nome: payload.nome,
+      descricao: payload.descricao,
+      preco: payload.preco,
+      unidade: payload.unidade,
+      posicao: payload.posicao,
+      ativo: payload.ativo,
+      categoria_id: payload.categoria_id,
+    };
+
+    if (payload.imagem) {
+      tmpProduct["imagem"] = await Drive.getUrl(payload.imagem.fileName!);
+    }
+
+    produto.merge(tmpProduct);
+    await produto.save();
+
+    return response.ok(produto);
   }
 
-  public async destroy({}: HttpContextContract) {
-    return "Hello World!";
+  public async destroy({ response, bouncer, params }: HttpContextContract) {
+    await bouncer.authorize("UserIsEstabelecimento");
+
+    const produto = await Produto.findOrFail(params.id);
+
+    await bouncer
+      .with("ProdutoPolicy")
+      .authorize("isOwner", produto.categoria_id);
+
+    produto.deletedAt = DateTime.local();
+    await produto.save();
+
+    return response.noContent();
   }
 }
